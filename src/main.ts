a7ff229e244e3d2918ba49b0362f8153e9e04382
Nguyen Duong <nduongg04@gmail.com>
2024-11-15 08:25:26 +0700

    Fix deploy action

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
-  const dbUrl = process.env.DATABASE_URL;
 
   await app.listen(port);
   console.log(`Server is running on port ${port}`);
-  console.log(`Database URL: ${dbUrl}`);
 }
 bootstrap();
